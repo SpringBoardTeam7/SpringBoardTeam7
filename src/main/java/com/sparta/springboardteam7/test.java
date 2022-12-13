0461f7371823df1931c317c58713b906ded20ffa
SHsus1122 <tjgurtn1122@naver.com>
2022-12-14 03:59:53 +0900

    test

---
+++ src/main/java/com/sparta/springboardteam7/test.java
@@ -0,0 +1,4 @@
+package com.sparta.springboardteam7;
+
+public class test {
+}
---
+++ src/test/java/com/sparta/springboardteam7/SpringBoardTeam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springboardteam7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardTeam7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
